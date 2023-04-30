fce0fcb430d1b07731fc5f9c21c9343b9f6451e6
ramdevan <ramdevan7@gmail.com>
2023-04-30 13:21:57 +0530

    message

---
+++ src/test/java/worker/Details.java
@@ -0,0 +1,16 @@
+package worker;
+
+public class Details {
+	private void sample1() {
+	  System.out.println("1");
+
+	}
+    private void sampl2() {
+	System.out.println("2");
+
+	}
+    private void sample3() {
+    System.out.println("3");
+
+	}
+}
